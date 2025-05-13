6cee09c19284d9007003583b51accdd923f80f8a
Hirak214 <53598504+hirak214@users.noreply.github.com>
2025-05-12 22:06:00 -0700

    Feat: basic .sh

--- basic.sh
+++ basic.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-g++ basic.cpp -o basic -std=c++17
-for input in datapoints/in*.txt; do
-    
-    num=$(basename "$input" | sed 's/in\([0-9]*\)\.txt/\1/')
-    
-    output="datapoints/out${num}.txt"
-
-    echo "Running on $input -> $output"
-    ./basic "$input" "$output"
-done
+python3 basic_3.py "$1" "$2"
